dcf539bee5f03b01ec3a9d1dcd05c6712e87b175
GlinZachariah <glincool.zac@gmail.com>
2019-04-03 10:20:47 +0530

    added admin site

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+admin.site.register(TSLA)
+admin.site.register(INTC)
+admin.site.register(QCOM)
+admin.site.register(NVDA)
+admin.site.register(MSFT)
+admin.site.register(IXIC)
