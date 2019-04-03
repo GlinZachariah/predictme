4979fa9ce336f41f5d699918b245438143e028fe
GlinZachariah <glincool.zac@gmail.com>
2019-04-03 10:09:23 +0530

    added sql query for stock

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+class TSLA(models.Model):
+    price_date= models.DateTimeField('date')
+    open_price = models.FloatField(default=0.0)
+    high_price = models.FloatField(default=0.0)
+    low_price = models.FloatField(default=0.0)
+    close_price = models.FloatField(default=0.0)
+    adj_close_price = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+
+class INTC(models.Model):
+    price_date= models.DateTimeField('date')
+    open_price = models.FloatField(default=0.0)
+    high_price = models.FloatField(default=0.0)
+    low_price = models.FloatField(default=0.0)
+    close_price = models.FloatField(default=0.0)
+    adj_close_price = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+
+class NVDA(models.Model):
+    price_date= models.DateTimeField('date')
+    open_price = models.FloatField(default=0.0)
+    high_price = models.FloatField(default=0.0)
+    low_price = models.FloatField(default=0.0)
+    close_price = models.FloatField(default=0.0)
+    adj_close_price = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+
+class QCOM(models.Model):
+    price_date= models.DateTimeField('date')
+    open_price = models.FloatField(default=0.0)
+    high_price = models.FloatField(default=0.0)
+    low_price = models.FloatField(default=0.0)
+    close_price = models.FloatField(default=0.0)
+    adj_close_price = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+
+class MSFT(models.Model):
+    price_date= models.DateTimeField('date')
+    open_price = models.FloatField(default=0.0)
+    high_price = models.FloatField(default=0.0)
+    low_price = models.FloatField(default=0.0)
+    close_price = models.FloatField(default=0.0)
+    adj_close_price = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+
+class IXIC(models.Model):
+    price_date= models.DateTimeField('date')
+    open_price = models.FloatField(default=0.0)
+    high_price = models.FloatField(default=0.0)
+    low_price = models.FloatField(default=0.0)
+    close_price = models.FloatField(default=0.0)
+    adj_close_price = models.FloatField(default=0.0)
+    volume = models.FloatField(default=0.0)
+
+# class NEWS(models.Model):
+#     news_date =models.DateTimeField('news date')
+#     news_ticker =models.DateTimeField('news date')
